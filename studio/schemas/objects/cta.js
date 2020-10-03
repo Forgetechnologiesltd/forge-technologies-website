5717e610960fd0a4efffa9fd8d9a40e850b48628
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-10-03 11:34:00 +0100

    Apply Prettier config

--- studio/schemas/objects/cta.js
+++ studio/schemas/objects/cta.js
@@ -1,73 +1,73 @@
 export default {
-  title: 'Call to action',
-  name: 'cta',
-  type: 'object',
+  title: "Call to action",
+  name: "cta",
+  type: "object",
   fieldsets: [
     {
-      title: 'Link',
-      name: 'link',
-      description: 'Only the first value of these will be used'
-    }
+      title: "Link",
+      name: "link",
+      description: "Only the first value of these will be used",
+    },
   ],
   fields: [
     {
-      title: 'Title',
-      name: 'title',
-      type: 'string'
+      title: "Title",
+      name: "title",
+      type: "string",
     },
     {
-      title: 'Landing page',
-      name: 'landingPageRoute',
-      type: 'reference',
-      fieldset: 'link',
-      to: [{type: 'route'}]
+      title: "Landing page",
+      name: "landingPageRoute",
+      type: "reference",
+      fieldset: "link",
+      to: [{ type: "route" }],
     },
     {
-      title: 'Path',
-      name: 'route',
-      fieldset: 'link',
-      description: 'Example: /blog',
-      type: 'string'
+      title: "Path",
+      name: "route",
+      fieldset: "link",
+      description: "Example: /blog",
+      type: "string",
     },
     {
-      title: 'External link',
-      name: 'link',
-      type: 'string',
-      description: 'Example: https://www.sanity.io',
-      fieldset: 'link'
+      title: "External link",
+      name: "link",
+      type: "string",
+      description: "Example: https://www.sanity.io",
+      fieldset: "link",
     },
     {
-      title: 'Kind',
-      name: 'kind',
-      type: 'string',
+      title: "Kind",
+      name: "kind",
+      type: "string",
       options: {
-        layout: 'radio',
-        list: ['button', 'link']
-      }
-    }
+        layout: "radio",
+        list: ["button", "link"],
+      },
+    },
   ],
   preview: {
     select: {
-      title: 'title',
-      landingPage: 'landingPageRoute.slug.current',
-      route: 'route',
-      link: 'link'
+      title: "title",
+      landingPage: "landingPageRoute.slug.current",
+      route: "route",
+      link: "link",
     },
-    prepare ({title, landingPage, route, link}) {
-      let subtitle = 'Not set'
+    prepare({ title, landingPage, route, link }) {
+      let subtitle = "Not set";
       if (landingPage) {
-        subtitle = `Route: /${landingPage}`
+        subtitle = `Route: /${landingPage}`;
       }
       if (route) {
-        subtitle = `Route: ${route}`
+        subtitle = `Route: ${route}`;
       }
       if (link) {
-        subtitle = `External: ${link}`
+        subtitle = `External: ${link}`;
       }
       return {
         title,
-        subtitle
-      }
-    }
-  }
-}
+        subtitle,
+      };
+    },
+  },
+};
