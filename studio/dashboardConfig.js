f33f3c593a4e5414b9e6c98befde6300d03b6f1c
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-10-03 15:41:16 +0100

    Add dashboard configuration

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,47 +1,56 @@
 export default {
   widgets: [
-    {name: 'structure-menu'},
+    { name: "structure-menu" },
     {
-      name: 'project-info',
+      name: "project-info",
       options: {
         __experimental_before: [
           {
-            name: 'netlify',
+            name: "netlify",
             options: {
               description:
-                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+                "NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.",
               sites: [
                 {
-                  buildHookId: '<#<deployments.studio.providerInfo.buildHookId>#>',
-                  title: 'Sanity Studio',
-                  name: '<#<deployments.studio.providerInfo.siteName>#>',
-                  apiId: '<#<deployments.studio.providerInfo.siteId>#>'
+                  buildHookId: "5f7851cab714641e47cbc596",
+                  title: "Forge Technologies Admin",
+                  name: "forge-technologies-admin",
+                  apiId: "347cbbfc-6369-4ace-8dfc-59e4c32b3113",
                 },
                 {
-                  buildHookId: '<#<deployments.web.providerInfo.buildHookId>#>',
-                  title: 'Blog Website',
-                  name: '<#<deployments.web.providerInfo.siteName>#>',
-                  apiId: '<#<deployments.web.providerInfo.siteId>#>'
-                }
-              ]
-            }
-          }
+                  buildHookId: "5f7851cab4d768471277abe1",
+                  title: "Forge Technologies Website",
+                  name: "forge-technologies",
+                  apiId: "54e87ec1-5383-4be2-b359-083c6c0cfbb2",
+                },
+              ],
+            },
+          },
         ],
         data: [
           {
-            title: 'GitHub repo',
-            value: 'https://github.com/<#<repository.owner>#>/<#<repository.name>#>',
-            category: 'Code'
+            title: "GitHub repo",
+            value:
+              "https://github.com/Forgetechnologiesltd/forge-technologies-website",
+            category: "Code",
           },
-          {title: 'Frontend', value: '<#<deployments.web.url>#>', category: 'apps'}
-        ]
-      }
+          {
+            title: "Frontend",
+            value: "https://forge-technologies.netlify.app/",
+            category: "apps",
+          },
+        ],
+      },
     },
-    {name: 'project-users', layout: {height: 'auto'}},
+    { name: "project-users", layout: { height: "auto" } },
     {
-      name: 'document-list',
-      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
-      layout: {width: 'medium'}
-    }
-  ]
-}
+      name: "document-list",
+      options: {
+        title: "Recent blog posts",
+        order: "_createdAt desc",
+        types: ["post"],
+      },
+      layout: { width: "medium" },
+    },
+  ],
+};
