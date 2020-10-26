96f97d96c824dcd476e2a2fbfcb3a52f50c20672
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-10-26 20:41:42 +0000

    Apply styles and layout to The Lover component

--- web/components/Lover.vue
+++ web/components/Lover.vue
@@ -1,22 +1,14 @@
 <template>
-  <section>
-    <div class="py-12"></div>
-
-    <v-container class="text-center">
-      <h2 class="display-2 font-weight-bold mb-3">{{ heading }}</h2>
-
-      <v-responsive class="mx-auto mb-8" width="56">
-        <v-divider class="mb-1"></v-divider>
-
-        <v-divider></v-divider>
-      </v-responsive>
-
-      <v-responsive
-        class="mx-auto title font-weight-light mb-8"
-        max-width="720"
-      >
-        <SanityContent :blocks="text" />
-      </v-responsive>
+  <section class="py-8">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="6" class="pr-16"
+          ><h2 class="mb-3 text-h4 pr-16">{{ heading }}</h2></v-col
+        >
+        <v-col cols="12" md="6">
+          <SanityContent :blocks="text" />
+        </v-col>
+      </v-row>
     </v-container>
   </section>
 </template>
