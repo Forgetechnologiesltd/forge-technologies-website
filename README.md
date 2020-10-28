58d7ed89135846f9e1025f1a321cce5c2e9f3adc
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-10-28 20:35:40 +0000

    Create The jester FE component
    Also fix a small mistake in the jester schema and in the description of the content type in the readme

--- README.md
+++ README.md
@@ -28,7 +28,7 @@
 - **The Caregiver**: Two of Heading, text, image and CTA
   ![Image of The Caregiver](images/caregiver.png)
 
-- **The Jester**: Icon, Heading, short text, image, CTA and a toggle to swap image and text sides
+- **The Jester**: Icon, Heading, short text, image and a toggle to swap image and text sides
   ![Image of The Jester](images/jester.png)
 
 - **The Everyman**: Content in tabs. Tab title and button to add other content types, as many as necessary.
---
+++ web/components/Jester.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <v-container fluid class="pa-0 mb-7">
+      <v-row no-gutters>
+        <v-col cols="12">
+          <v-img
+            :min-height="'calc(32vh)'"
+            :max-height="'calc(32vh)'"
+            :src="$imageHelper(image).url()"
+            gradient="rgba(0,0,0,.4), rgba(0,0,0,.5)"
+          >
+            <v-theme-provider dark>
+              <v-container fill-height>
+                <v-row
+                  :align="side"
+                  class="white--text mx-auto"
+                  justify="center"
+                >
+                  <v-col
+                    class="white--text offset-md-1 px-0"
+                    :class="[side === 'right' ? ' text-right ' : ' text-left ']"
+                    xs="6"
+                  >
+                    <v-icon large color="white"> mdi-{{ icon.icon }} </v-icon>
+                    <h4 class="mb-0 text-h5 text-md-h4 pl-0">
+                      {{ heading }}
+                    </h4>
+                    <SanityContent
+                      class="text-body-2 col-md-7 pl-0"
+                      :class="[
+                        side === 'right'
+                          ? ' text-right ml-auto '
+                          : ' text-left ',
+                      ]"
+                      :blocks="shortText"
+                    />
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-theme-provider>
+          </v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Jester',
+  props: {
+    heading: String,
+    shortText: Array,
+    image: Object,
+    side: String,
+    icon: Object,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  max-width: 100%;
+}
+</style>
