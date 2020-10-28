d089dcf3fcb38f259cb08bc92bab74b9975eb541
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-10-28 20:37:25 +0000

    Add some changes to the Magician FE component

--- web/components/Magician.vue
+++ web/components/Magician.vue
@@ -1,18 +1,28 @@
 <template>
-  <v-container>
+  <v-container fluid class="pa-0 mb-7 mt-md-7" fill-height>
     <v-row>
-      <v-col cols="6">
-        <SanityImage
-          v-if="mediumSizeImage"
-          :asset-id="mediumSizeImage.asset._ref"
-          auto="format"
-        />
+      <v-col cols="6" class="ml-n16">
+        <v-img
+          :min-height="'calc(50vh)'"
+          :max-height="'calc(50vh)'"
+          :src="$imageHelper(mediumSizeImage).url()"
+          gradient="rgba(0,0,0,.4), rgba(0,0,0,.5)"
+        >
+        </v-img>
       </v-col>
-      <v-col cols="6">
-        <h1>{{ heading }}</h1>
-        <SanityContent :blocks="paragraph" />
-        {{ cta }}
+      <v-col class="" cols="6" align-center>
+        <h4 class="mb-0 text-h5 text-md-h4 pl-0">
+          {{ heading }}
+        </h4>
+        <SanityContent class="text-body-2 col-md-12 pl-0" :blocks="paragraph" />
+        <v-btn v-if="cta" depressed color="primary" nuxt :to="cta.slug">{{
+          cta.title
+        }}</v-btn>
       </v-col>
+      <!-- <v-container fill-height>
+        <v-row align="left" class="mx-auto" justify="center">
+        </v-row>
+      </v-container> -->
     </v-row>
   </v-container>
 </template>
