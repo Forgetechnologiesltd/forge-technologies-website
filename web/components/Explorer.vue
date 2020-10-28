a3f27f00fe26bd36cb6c8aeb550275cb1e488267
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-10-28 20:36:39 +0000

    Create The Explorer FE Component
    Unfinished

--- web/components/Explorer.vue
+++ web/components/Explorer.vue
@@ -1,20 +1,28 @@
 <template>
-<div>
-  {{explorerItems}}
-</div>
+  <div>
+    <v-carousel light>
+      <v-carousel-item v-for="item in explorerItems" :key="item._key">
+        <v-row class="fill-height" align="center" justify="center">
+          <h3 class="text-h3">
+            {{ item.heading }}
+          </h3>
+        </v-row>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: "Explorer",
+  name: 'Explorer',
   props: {
-      explorerItems: Array,
-  }
+    explorerItems: Array,
+  },
 }
-
 </script>
 
 <style lang="scss" scoped>
-img{max-width: 100%;}
+img {
+  max-width: 100%;
+}
 </style>
