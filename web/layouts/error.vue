22678e0b5abf182b2d7c9b62e16707d7066b51d6
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-11-18 15:09:54 +0000

    Display logo on error page

--- web/layouts/error.vue
+++ web/layouts/error.vue
@@ -1,39 +1,50 @@
 <template>
-  <v-app >
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+  <v-app>
+    <v-container fill-height fluid>
+      <v-row align="center" justify="center">
+        <v-col>
+          <v-card flat class="col-6 mx-auto">
+            <forge-logo></forge-logo>
+            <h1 v-if="error.statusCode === 404">
+              {{ pageNotFound }}
+            </h1>
+            <h1 v-else>
+              {{ otherError }}
+            </h1>
+            <NuxtLink to="/"> Home page </NuxtLink>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
+import ForgeLogo from '~/components/ForgeLogo'
 export default {
   layout: 'empty',
   props: {
     error: {
       type: Object,
-      default: null
-    }
+      default: null,
+    },
   },
-  data () {
+  components: {
+    ForgeLogo,
+  },
+  data() {
     return {
       pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      otherError: 'An error occurred',
     }
   },
-  head () {
+  head() {
     const title =
       this.error.statusCode === 404 ? this.pageNotFound : this.otherError
     return {
-      title
+      title,
     }
-  }
+  },
 }
 </script>
 
