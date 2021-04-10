8380f0da5e389fe1cedac8981d4488fc7fbe3ab2
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2021-04-10 07:36:53 +0100

    Enable google crawling

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -28,7 +28,7 @@ export default async (ctx) => {
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { hid: 'description', name: 'description', content: '' },
-        { hid: 'robots', name: 'robots', content: 'noindex' },
+        { hid: 'robots', name: 'robots', content: 'index, follow' },
         {
           hid: 'og:title',
           property: 'og:title',
@@ -97,7 +97,7 @@ export default async (ctx) => {
     },
     robots: {
       UserAgent: '*',
-      Disallow: '/',
+      Allow: '/',
     },
     // privateRuntimeConfig: {
     //   sanity: {
