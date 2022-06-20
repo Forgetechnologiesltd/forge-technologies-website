e98977cccb505859f06fca1efa37a45460c7dd61
Salih Demirel <salih.demirel@hangikredi.com>
2022-06-20 13:56:57 +0300

    add some meta tags

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -24,7 +24,11 @@ export default async (ctx) => {
     head: {
       titleTemplate: '%s | Forge Technologies',
       title: siteSettings.title,
-     
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'robots', name: 'robots', content: 'index, follow' },
+      ],
       script:[
         {
           type:'text-javascript',
