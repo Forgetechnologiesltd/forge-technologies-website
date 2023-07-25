8fa2320ac514ce0137a926c00dbeda19eccd2d89
Salih Demirel <salih.demirel@hangikredi.com>
2023-07-25 17:36:37 +0300

    fix youtube error.

--- web/components/Youtube.vue
+++ web/components/Youtube.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        <youtube :video-id="videoId"></youtube> 
+      <!--  <youtube :video-id=""></youtube> -->
+        
+      <iframe 
+        title="Youtube Preview"
+        width="560" 
+        height="315" 
+        :src="'https://www.youtube.com/embed/' + videoId"
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    />
+    
     </div>
 </template>
 
